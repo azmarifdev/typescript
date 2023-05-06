a5328fa32c7eb8b5844a25a39d4e9168a9a0ce83
azmarifcode <azmarif.dev@gmail.com>
2023-05-06 19:01:05 +0600

    module 2

--- Module-2/src/question-mark.ts
+++ Module-2/src/question-mark.ts
@@ -1,1 +1,51 @@
-  
+const age: number = 18;
+
+if (age >= 25) {
+    // console.log('old');
+} else if (age >= 18) {
+    // console.log('adult');
+} else {
+    // console.log('child');
+}
+
+if (age >= 18) {
+    // console.log('adult');
+} else {
+    // console.log('child');
+}
+
+const result = age >= 18 ? 'adult' : 'child';
+// console.log('result -->', result);
+
+// null and undefined
+const isAuthenticatedUser = '';
+const userName = isAuthenticatedUser ?? 'Guest';
+const userName2 = isAuthenticatedUser ? isAuthenticatedUser : 'Guest';
+// console.log({ userName }, { userName2 });
+
+type man = {
+    name: string;
+    age: number;
+    address: {
+        city: 'No city';
+        road: 'No road';
+        home?: '';
+    };
+};
+
+const man1: man = {
+    name: 'arif',
+    age: 18,
+    address: {
+        city: 'No city',
+        road: 'No road',
+    },
+};
+
+const home = man1?.address?.home ?? 'No home';
+console.log('home', home);
+
+let obj: Object = {key: 'value'}
+let obj2: {} = {key: 'value'}
+console.log("obj", obj);
+console.log("obj2", obj2);
