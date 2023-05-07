51ed14e3a30a1925125515356432424b8136c12f
azmarifcode <azmarif.dev@gmail.com>
2023-05-07 23:37:31 +0600

    module 3 generic func

--- Module-2/src/Module-3/Generic/generic-function.ts
+++ Module-2/src/Module-3/Generic/generic-function.ts
@@ -11,9 +11,12 @@ const genFunc2 = <X, Y>(param1: X, param2: Y): [X, Y] => {
     return [param1, param2];
 };
 
+type user = { name: string; age: number };
+
 const result = genFunc('arif');
 const result1 = genFunc1<string>('bangladesh');
 const result2 = genFunc1<number>(56);
 const result3 = genFunc1<boolean>(true);
 const result4 = genFunc2<string, number>('azmarif', 45);
 const result5 = genFunc2<boolean, Array<number>>(true, [85, 98, 23]);
+const result6 = genFunc2<user, boolean>({ name: 'arif', age: 23 }, false);
