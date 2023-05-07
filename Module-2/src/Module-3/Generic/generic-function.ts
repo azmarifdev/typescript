034016fbc6355d91f98fc08b89c829bae7507ea4
azmarifcode <azmarif.dev@gmail.com>
2023-05-08 04:16:35 +0600

    module 3 generic con

--- Module-2/src/Module-3/Generic/generic-function.ts
+++ Module-2/src/Module-3/Generic/generic-function.ts
@@ -1,3 +1,4 @@
+/*
 // arrow function
 const genFunc = (param: string): string => {
     return param;
@@ -7,6 +8,12 @@ const genFunc1 = <T>(param: T): T[] => {
     return [param];
 };
 
+// function
+// function genFunc2<X, Y>(param1: X, param2: Y): [X, Y] {
+//     return [param1, param2];
+// }
+
+// Arrow Function
 const genFunc2 = <X, Y>(param1: X, param2: Y): [X, Y] => {
     return [param1, param2];
 };
@@ -20,3 +27,37 @@ const result3 = genFunc1<boolean>(true);
 const result4 = genFunc2<string, number>('azmarif', 45);
 const result5 = genFunc2<boolean, Array<number>>(true, [85, 98, 23]);
 const result6 = genFunc2<user, boolean>({ name: 'arif', age: 23 }, false);
+
+/// spread operator default
+
+const crush = 'kitty';
+const me = {
+    name: 'arif',
+    age: 33,
+};
+const spread = { ...me, crush };
+
+/// spread operator function
+
+const addMeInMyCrushMind = <T>(param: T): T => {
+    const crushName = 'kitty';
+    const newData = { ...param, crushName };
+    return newData;
+};
+
+type Info = {
+    name: string;
+    age: number;
+    salary: string;
+    married: boolean;
+};
+
+const myInfo: Info = {
+    name: 'arif',
+    age: 30,
+    salary: '20000 tk',
+    married: false,
+};
+
+const result7 = addMeInMyCrushMind(myInfo);
+*/
